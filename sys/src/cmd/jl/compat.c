b51ec28218e688888ee85b38556a4d00de124dfc
Jacob Moody <moody@posixcafe.org>
2024-04-24 19:19:09 +0000

    ja, jc, jl: import from 9p.io
    Only changes are moving from i* to j*
    and some minor mkfile cleaning.
    No code changes, so these do not work as-is yet.

---
+++ sys/src/cmd/jl/compat.c
@@ -0,0 +1,67 @@
+#include	"l.h"
+
+/*
+ * fake malloc
+ */
+void*
+malloc(uintptr n)
+{
+	void *p;
+
+	while(n & 7)
+		n++;
+	while(nhunk < n)
+		gethunk();
+	p = hunk;
+	nhunk -= n;
+	hunk += n;
+	return p;
+}
+
+void*
+mallocz(uintptr n, int clr)
+{
+	void *p;
+
+	while(n & 7)
+		n++;
+	while(nhunk < n)
+		gethunk();
+	p = hunk;
+	if (clr)
+		memset(p, 0, n);
+	nhunk -= n;
+	hunk += n;
+	return p;
+}
+
+void
+free(void *p)
+{
+	USED(p);
+}
+
+void*
+calloc(uintptr m, uintptr n)
+{
+	void *p;
+
+	n *= m;
+	p = malloc(n);
+	memset(p, 0, n);
+	return p;
+}
+
+void*
+realloc(void*, uintptr)
+{
+	fprint(2, "realloc called\n");
+	abort();
+	return 0;
+}
+
+void
+setmalloctag(void *v, uintptr pc)
+{
+	USED(v, pc);
+}
