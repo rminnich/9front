a9e10074eb16467da53ebe70dd98f458b2f95692
Ori Bernstein <ori@eigenstate.org>
2025-11-03 04:48:31 +0000

    gefs: remove debug junk

--- sys/src/cmd/gefs/tree.c
+++ sys/src/cmd/gefs/tree.c
@@ -549,7 +549,6 @@ updateleaf(Tree *t, Path *up, Path *p)
 			ok = 0;
 			if(m.op != Oclearb && m.op != Oclobber){
 				/* New keys need to start off with Oinsert */
-if(m.op != Oinsert) fprint(2, "not insert: [%d] %M\n", m.op, &m);
 				assert(m.op == Oinsert);
 				spc -= valsz(&m);
 				p->pullsz += msgsz(&m);
@@ -756,7 +755,6 @@ splitleaf(Tree *t, Path *up, Path *p, Kvp *mid)
 			ok = 0;
 			if(m.op != Oclearb && m.op != Oclobber){
 				/* New keys need to start off with Oinsert */
-if(m.op != Oinsert) fprint(2, "not insert: %d\n", m.op);
 				assert(m.op == Oinsert);
 				spc -= valsz(&m);
 				p->pullsz += msgsz(&m);
@@ -1553,7 +1551,6 @@ Again:
 		getval(p[h-1].b, p[h-1].vi, &m);
 	}else{
 		getmsg(p[start-1].b, p[start-1].bi, &m);
-if(m.op != Oinsert) fprint(2, "not insert: %d\n", m.op);
 		assert(m.op == Oinsert);
 		bufsrc = start-1;
 	}
