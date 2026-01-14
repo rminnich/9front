01d2ed14d85f0eb6b69e7d0e5d9195de9d90bc7a
g.lenda <rminnich@me.com>
2026-01-14 20:58:40 +0000

    more stuff from geoff. Dying in pagefault, stval seems to get trashed somehow? not clear.

--- sys/src/9/riscv64/trap.c
+++ sys/src/9/riscv64/trap.c
@@ -11,6 +11,10 @@
 #include "riscv64.h"
 #include "/sys/src/libc/9syscall/sys.h"
 
+/* instruction decoding */
+#define UNCOMPINST(pc)	(*(ushort *)(pc) | *(ushort *)((pc) + 2) << 16)
+#define BASEOP(inst)	((inst) & MASK(7))
+
 enum {
 	Trapdebug	= 1,
 	Probedebug	= 0,
@@ -229,6 +233,48 @@ static char* excname[] = {
 */
 };
 
+static void
+trapdbg(Ureg *ureg, Cause *cp, int entry)
+{
+	int type;
+
+	type = cp->type;
+	/* if we print uart interrupts, we'll recurse forever. */
+//	if (type == Globalintr)
+//		return;
+/*
+	iprint("|%c%c%c ", ureg->curmode == Mppsuper? 'S': 'M', entry? '>': '<',
+		type == Exception? 'E': 'I');
+*/
+	if (cp->cause >= nelem(excname) || excname[cp->cause] == nil)
+		iprint("cause %d", cp->cause);
+	else {
+		iprint("%s", excname[cp->cause]);
+		if (cp->cause == Envcalluser) {
+			iprint(" pid %d", up->pid);
+			if (entry)
+				iprint(" %d", ureg->arg);
+			else
+				iprint(" return %#llux", ureg->arg);
+		}
+	}
+	iprint(" from %s pc %#p tval %#p up %#p\n",
+		cp->user? "user": "kernel", ureg->pc, ureg->tval, up);
+}
+
+static void
+posttrapnote(Ureg *ureg, uint cause, char *name)
+{
+	char buf[ERRMAX];
+
+	spllo();
+	snprint(buf, sizeof buf, "sys: trap: %s for address %#p",
+		cause < nelem(excname) && excname[cause]? excname[cause]: name,
+		ureg->tval);
+	postnote(up, 1, buf, NDebug);
+}
+
+
 /*
  * on riscv, we have to manually advance the PC past an ECALL instruction,
  * for example, so that we don't re-execute it.
@@ -272,33 +318,35 @@ trapsyscall(Ureg *ureg, Cause *)
 }
 
 static void
-trapdbg(Ureg *ureg, Cause *cp, int entry)
+trapcallmch(Ureg *, Cause *)
 {
-	int type;
-
-	type = cp->type;
-	/* if we print uart interrupts, we'll recurse forever. */
-//	if (type == Globalintr)
-//		return;
-/*
-	iprint("|%c%c%c ", ureg->curmode == Mppsuper? 'S': 'M', entry? '>': '<',
-		type == Exception? 'E': 'I');
-*/
-	if (cp->cause >= nelem(excname) || excname[cp->cause] == nil)
-		iprint("cause %d", cp->cause);
-	else {
-		iprint("%s", excname[cp->cause]);
-		if (cp->cause == Envcalluser) {
-			iprint(" pid %d", up->pid);
-			if (entry)
-				iprint(" %d", ureg->arg);
-			else
-				iprint(" return %#llux", ureg->arg);
-		}
-	}
-	iprint(" from %s pc %#p tval %#p up %#p\n",
-		cp->user? "user": "kernel", ureg->pc, ureg->tval, up);
+	panic("unexpected environment call from machine mode");
 }
+
+static void
+trapmisaligned(Ureg *ureg, Cause *cp)
+{
+	if (cp->user)
+		posttrapnote(ureg, cp->cause, "misaligned access");
+	else
+		panic("misaligned access to %#p at %#p", ureg->tval, ureg->pc);
+}
+
+static void
+debugbpt(Ureg* , Cause*)
+{
+	char buf[ERRMAX];
+
+	if(up == 0)
+		panic("kernel bpt");
+	/*
+	 * on riscv, pc points at the instruction that caused the trap,
+	 * so there's no need to back up the pc.
+	 */
+	snprint(buf, sizeof buf, "sys: breakpoint");
+	postnote(up, 1, buf, NDebug);
+}
+
 
 /* decode trap cause from *ureg into *cp & return type */
 static int
@@ -338,9 +386,256 @@ whatcause(Cause *cp, Ureg *ureg)
 	return type;
 }
 
+static void
+trapaccess(Ureg *ureg, Cause *cp)
+{
+	if (cp->user)
+		posttrapnote(ureg, cp->cause, "illegal access");
+	else
+		panic("trap: illegal access to %#p at %#p", ureg->tval,
+			ureg->pc);
+}
+
+static void
+badinst(Ureg *ureg, Cause *cp)
+{
+	if (cp->user)
+		posttrapnote(ureg, cp->cause, "illegal instruction");
+	else
+		panic("illegal instruction at %#p: %#p", ureg->pc, ureg->tval);
+}
+
+/*
+ * Illinst could be from legitimate user fp use while fpu is off.
+ * Bad CSR accesses will be noted and ignored.
+ * It could also be due to the C910 erroneously trapping an unknown fence,
+ * such as fence.tso.
+ */
+static void
+trapillinst(Ureg *ureg, Cause *cp)
+{
+	int rd, skip, funct3;
+	ulong inst;
+	uintptr pc;
+
+	/* if non-zero, ureg->tval will be the trapping instruction */
+	pc = ureg->pc;
+	if (pc & 1) {
+		if (!cp->user)
+			panic("trapillinst: odd pc %#p", pc);
+		ureg->tval = pc;
+		posttrapnote(ureg, cp->cause, "odd pc");
+		return;
+	}
+
+	print("IMPLEMENT ISFPINST\n");
+#ifdef XXX
+	if (isfpinst(pc, ureg)) {
+		if (!cp->user)
+			panic("kernel fpu use at %#p: %#p", pc, ureg->tval);
+	//	fptrap(ureg, 0);
+		vecacct(vctl[cp->vno]);
+		return;			/* re-execute FP but with FPU on */
+	}
+#endif
+	/*
+	 * if we support vector extension, we'll need to cope with the
+	 * vector instructions and vector processor here, analogously to fp.
+	 * [ms]status.Vsst is vector context status.  ja and jl will have to
+	 * implement the vector instructions first.
+	 */
+
+	if (ISCOMPRESSED(*(ushort *)pc)) {	/* instrs are little-endian */
+		badinst(ureg, cp);
+		return;
+	}
+	skip = 0;
+	inst = UNCOMPINST(pc);
+	if (inst != ureg->tval)
+		iprint("illinst: inst %#lux tval %#llux\n", inst, ureg->tval);
+	funct3 = (inst>>12) & MASK(3);
+	switch (BASEOP(inst)) {
+	case SYSTEM:
+		if ((funct3 & MASK(2)) != 0) {
+			/*
+			 * bad CSR: zero dest and otherwise ignore.
+			 * some CSRs are optional or obsolete.
+			 * it could be a CSR that we want to emulate, in theory.
+			 */
+			iprint("CSR instruction for bad CSR %lx at %#p\n",
+				inst>>20, pc);
+			rd = (inst>>7) & MASK(5);
+			if (rd)
+				ureg->regs[rd] = 0;
+		//	m->probebad = 1;
+			skip = 1;
+		}
+		break;
+	case 0xf:
+		if (funct3 == 0)
+			/*
+			 * some fence; shouldn't happen, but c910 is buggy.
+			 * coherence has been called.
+			 */
+			skip = 1;
+		break;
+	}
+	if (skip)
+		ureg->pc += 4;
+	else
+		badinst(ureg, cp);
+}
+
+/*
+ * the page fault couldn't be resolved, probably because the address is
+ * unmapped.  Report the error in the appropriate way.
+ */
+static void
+badpagefault(Ureg *ureg, uintptr addr, int read, int insyscall)
+{
+	char buf[ERRMAX];
+
+	/*
+	 * It is possible to get here with !user if, for example,
+	 * a process was in a system call accessing a shared
+	 * segment but was preempted by another process which shrunk
+	 * or deallocated the shared segment; when the original
+	 * process resumes it may fault while in kernel mode.
+	 * No need to panic this case, post a note to the process
+	 * and unwind the error stack. There must be an error stack
+	 * (up->nerrlab != 0) if this is a system call, if not then
+	 * the game's a bogey.
+	 */
+	if(!userureg(ureg) && (!insyscall || up->nerrlab == 0)){
+		dumpregs(ureg);
+		panic("fault: addr %#p pc %#p", addr, ureg->pc);
+	}
+	snprint(buf, sizeof buf, "sys: trap: fault %s addr=%#p pc=%#p",
+		read? "read": "write", addr, ureg->pc);
+	postnote(up, 1, buf, NDebug);
+	if(insyscall)
+		error(buf);
+}
+
+/*
+ *  find out fault address and type of access.
+ *  Call common fault handler.
+ */
+static void
+faultriscv64(Ureg* ureg, Cause *cp)
+{
+	uintptr addr;
+	int read, insyscall;
+	extern int block;
+
+	/*
+	 * There must be a user context.
+	 * If not, the usual problem is causing a fault during
+	 * initialisation before the system is fully up.
+	 */
+	addr = ureg->tval;
+	if(up == nil)
+		panic("fault %#lld with up == nil; pc %#p addr %#p",
+			ureg->cause, ureg->pc, addr);
+	if (addr == 0 && up->nlocks)		/* debugging for gs */
+		panic("fault %#lld pc %#p addr %#p nlocks %d",
+			ureg->cause, ureg->pc, addr, up->nlocks);
+
+	//vecacct(vctl[cp->vno]);
+	insyscall = up->insyscall;
+	up->insyscall = 1;
+	read = ureg->cause != Storepage;  /* exception, so Rv64intr must be 0 */
+	/* page fault on a kernel address is never okay. */
+	while (!block);
+	if((intptr)addr < 0 || fault(addr, ureg->pc, read) < 0)
+		badpagefault(ureg, addr, read, insyscall);
+	up->insyscall = insyscall;
+}
+
+typedef struct Lastexcept Lastexcept;
+struct Lastexcept {		/* should be per cpu */
+	uintptr	pc;
+	uintptr	addr;		/* failed address of load or store */
+	short	consec;
+};
+
+static void
+faultstuck(Ureg *ureg)
+{
+	static Lastexcept exc;
+
+	if (ureg->pc == exc.pc && ureg->tval == exc.addr) {
+		if (++exc.consec >= 10)
+			panic("%d consecutive exceptions at pc %#p for addr %#p",
+				exc.consec, exc.pc, exc.addr);
+	} else {
+		exc.pc = ureg->pc;
+		exc.addr = ureg->tval;
+		exc.consec = 0;
+	}
+}
+
+/* see trap() for Envcalluser short cut, mtrap.s for Envcallsup */
+static Exchandler exchandlers[] = {
+[Breakpt]	debugbpt,
+[Instpage]	faultriscv64,		/* page fault */
+[Loadpage]	faultriscv64,
+[Storepage]	faultriscv64,
+[Instaccess]	trapaccess,		/* failed pmp or pma check */
+[Loadaccess]	trapaccess,
+[Storeaccess]	trapaccess,
+[Illinst]	trapillinst,
+[Instaddralign]	trapmisaligned,
+[Loadaddralign]	trapmisaligned,
+[Storeaddralign] trapmisaligned,
+[Envcalluser]	trapsyscall,		/* backstop; short-cut in trap */
+[Envcallmch]	trapcallmch,
+/*
+ * missing: Envcallvsup, Debugexc, Instgpage, Loadgpage, Virtinstr, Storegpage,
+ * Doubletrap, Swchk, Hwerr
+ */
+};
+
+/* handle exceptions */
+static int
+trapriscv64(Ureg *ureg, Cause *cp)
+{
+	uint cause;
+	Exchandler handler;
+
+#ifdef xxx
+	if (cp->user)
+		m->turnedfpoff = 0;
+	else if (m->probing) {
+		m->probebad = 1;
+		m->probing = 0;
+		coherence();
+		if (0)
+			iprint("probe trapped\n");
+		/* have to advance PC on risc-v to skip faulting instruction. */
+		advancepc(ureg);
+		return 0;			/* not a clock interrupt */
+	}
+#endif
+
+	cause = cp->cause;
+	if (cause >= nelem(exchandlers))
+		panic("trapriscv64: cause %d out of range", cause);
+	handler = exchandlers[cause];
+	if (handler) {
+		(*handler)(ureg, cp);
+		if (Trapdebug)
+			faultstuck(ureg);
+		return 0;			/* not a clock interrupt */
+	}
+
+	/* could be a local intr */
+	panic("unknown exception, cause %d from_user %d", cause, cp->user);
+}
+
 static Traphandler traphandlers[Nfaulttypes] = {
 [Unknownflt]	nil,
-//[Exception]	trapriscv64,			/* may enable fpu */
+[Exception]	trapriscv64,			/* may enable fpu */
 //[Localintr]	traplocalintr,
 //[Globalintr]	intr,
 };
