1e84e992f11fe25f7008e5427cd5573f6c7e3335
g.lenda <rminnich@me.com>
2026-02-19 16:53:42 +0000

    turn random back on

--- sys/src/9/port/random.c
+++ sys/src/9/port/random.c
@@ -92,8 +92,8 @@ randominit(void)
 	print("randominit\n");
 	rs = secalloc(sizeof(*rs));
 	qlock(rs);	/* randomseed() unlocks once seeded */
-	if (0) kproc("randomseed", randomseed, nil);
-	print("BE SURE TO TURN RANDOMINIT BACK ON\n");
+	if (1) kproc("randomseed", randomseed, nil);
+	else print("BE SURE TO TURN RANDOMINIT BACK ON\n");
 	print("randominit: done\n");
 }
 
